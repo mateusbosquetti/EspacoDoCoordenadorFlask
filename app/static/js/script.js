2d5f7121e217722fb468895cc265eb139cabe4e0
Mateus Henrique Bosquetti <mateushb123@gmail.com>
2024-08-01 11:00:46 -0300

    a

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,1 +1,25 @@
-console.log('Hello world, vai tomar sopa de canudinho')
+document.addEventListener('DOMContentLoaded', function () {
+    const $html = document.querySelector('html');
+    const $button = document.querySelector('#dark-mode-toggle');
+
+    // Carregar a preferência de dark mode do localStorage
+    const darkMode = localStorage.getItem('dark-mode');
+    if (darkMode === 'enabled') {
+        $html.classList.add('dark-mode');
+        $button.textContent = 'Light Mode';
+    } else {
+        $button.textContent = 'Dark Mode';
+    }
+
+    // Adicionar um event listener para alternar o dark mode
+    $button.addEventListener('click', function () {
+        $html.classList.toggle('dark-mode');
+        if ($html.classList.contains('dark-mode')) {
+            localStorage.setItem('dark-mode', 'enabled');
+            $button.textContent = 'Light Mode';
+        } else {
+            localStorage.setItem('dark-mode', 'disabled');
+            $button.textContent = 'Dark Mode';
+        }
+    });
+});
